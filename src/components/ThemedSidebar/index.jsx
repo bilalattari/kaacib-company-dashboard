30f98a7891bd71dfa5ddd830168e90c7abaf3f39
Ahmed Shahid <a.razashahid19@gmail.com>
2025-11-04 00:35:52 +0500

    add: themed sidebar

--- src/components/ThemedSidebar/index.jsx
+++ src/components/ThemedSidebar/index.jsx
@@ -1,47 +1,92 @@
 import { useState } from 'react';
-import {
-  AppstoreOutlined,
-  ContainerOutlined,
-  DesktopOutlined,
-  MailOutlined,
-  PieChartOutlined,
-} from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 import { Menu } from 'antd';
+import {
+  ChevronLeft,
+  GitForkIcon,
+  Package,
+  PieChart,
+  TicketsIcon,
+  Users,
+  Wrench,
+} from 'lucide-react';
+import './index.css';
 
 const items = [
-  { key: '1', icon: <PieChartOutlined />, label: 'Option 1' },
-  { key: '2', icon: <DesktopOutlined />, label: 'Option 2' },
-  { key: '3', icon: <ContainerOutlined />, label: 'Option 3' },
   {
-    key: 'sub1',
-    label: 'Navigation One',
-    icon: <MailOutlined />,
+    icon: <PieChart className="size-5" />,
+    label: 'Dashboard',
+    path: '/dashboard',
   },
   {
-    key: 'sub2',
-    label: 'Navigation Two',
-    icon: <AppstoreOutlined />,
+    icon: <TicketsIcon className="size-5" />,
+    label: 'Tickets',
+    path: '/tickets',
+  },
+  {
+    icon: <Wrench className="size-5" />,
+    label: 'Workers',
+    path: '/workers',
+  },
+  {
+    icon: <GitForkIcon className="size-5" />,
+    label: 'Branches',
+    path: '/branches',
+  },
+  {
+    icon: <Package className="size-5" />,
+    label: 'Assets',
+    path: '/assets',
+  },
+  {
+    icon: <Users className="size-5" />,
+    label: 'Users',
+    path: '/users',
   },
 ];
 
 const ThemedSidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
 
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
 
   return (
-    <div className="w-full h-full flex flex-col border-2 border-red-600">
-      <div className="flex-content-center bg-white rounded-full w-4 h-4">
-        <img
-          src="/logo.png"
-          alt="Logo"
-          className="w-full h-full bg-transparent"
-        />
+    <div className="theme-bg flex-content-center flex-col w-full h-full pt-4 gap-4 border-2 border-red-600 px-1">
+      <div
+        onClick={toggleCollapsed}
+        className={`${
+          collapsed
+            ? ' flex-content-center w-12 rounded-full'
+            : 'flex items-center justify-around w-full rounded-xl'
+        } h-12 bg-white shadow-md cursor-pointer transition-all duration-500`}
+      >
+        {collapsed ? (
+          <img src="/logo.png" alt="Logo" className="w-2/3 h-2/3 object-fill" />
+        ) : (
+          <>
+            <p className="font-semibold text-md">KAACIB</p>
+            <ChevronLeft className="size-6" />
+          </>
+        )}
       </div>
 
-      <Menu className="theme-bg w-full h-full" items={items} />
+      <div onClick={toggleCollapsed} className=" h-12 bg-white"></div>
+
+      <Menu className="theme-bg w-full h-full" inlineCollapsed={collapsed}>
+        {items.map((item, i) => (
+          <Menu.Item
+            key={i}
+            icon={item.icon}
+            onClick={() => navigate(item.path)}
+            className="flex items-center justify-between text-white! mb-2! hover:bg-white/90! hover:text-black! hover:shadow-md! hover:translate-y-1  transition-all duration-300"
+          >
+            <span className="text-md">{item.label}</span>
+          </Menu.Item>
+        ))}
+      </Menu>
     </div>
   );
 };
